a28d63f5a62378ebcd73ca111ffcc8897c22eec6
MikalMirkas <MikalMirkas@users.noreply.github.com>
2019-05-24 08:17:39 -0500

    update all existing modules for new model

--- modules/commands/booru/search.js
+++ modules/commands/booru/search.js
@@ -2,24 +2,45 @@
 /**
  * @author Michael Mirkas
  * @date May 22nd, 2019
- * @updated May 22nd, 2019
+ * @updated May 23rd, 2019
  * @desc Returns an SFW image from a booru.
  */
 
- const Booru = require('booru');
 
- //Safebooru
- /**
-  * @param {*} query Text to search for.
-  * @returns A Post object.
-  */
- const booruSearch = async (query = "Mahira_(granblue_fantasy)") => {
-    return await Booru.search('safebooru', query.trim().split(/\s+/), {limit: 1, random: true})
-    .then(posts => {
-        for (let post of posts)
-            return post;
-        })
- };
+const Booru = require('booru');
+const { RichEmbed } = require('discord.js');
 
- 
-exports.search = booruSearch;
+exports.run = async (client, message, args) => {
+    try {
+        if (args.length == 0 || !Array.isArray(args)) {
+            throw ("This command requires additional parameters.");
+        }
+        return await Booru.search('safebooru', args.join(' '), { limit: 1, random: true })
+            .then(posts => {
+                for (let post of posts)
+                    return post;
+            })
+            .then(function (post) {
+                console.log(post);
+                message.channel.send(
+                    new RichEmbed()
+                        .setImage(post.fileUrl)
+                        .setURL(post.postView)
+                        .setTitle("Mahiraposting")
+                        .setColor(0xE159F5)
+                        .setFooter("Generated on " + new Date().toUTCString() + " via booru.js.", client.user.avatarURL)
+                );
+            })
+            .catch((err) => {
+                message.channel.send(err.message);
+            });
+    }
+    catch (e) {
+        message.channel.send(e);
+    }
+};
+
+exports.command = {
+    "alias": "imageboard",
+    "description": "Returns an SFW image from a booru."
+}
--- modules/commands/wiki/search.js
+++ modules/commands/wiki/search.js
@@ -1,7 +1,7 @@
 /**
  * @author Michael Mirkas
  * @date May 19th, 2019
- * @updated May 20th, 2019
+ * @updated May 24th, 2019
  * @desc Powers the search functionality of MediaWiki applications.
  * @notes urllib does not support SPAs.
  */
@@ -19,13 +19,13 @@ const search = async (wikiRootUrl, query, filter = " - ") => {
         return "This command requires additional parameters.";
     }
     else {
-        var encodedUrl = wikiRootUrl + query.replace(/ /g, "_");
+        var encodedUrl = wikiRootUrl + query;
         
         return await urllib.request(encodedUrl)
         .then(function(result) {
             var reply;
             //result: {data: buffer, res: response object}
-            //console.log('parameters: %s, status: %s, body size: %d', query, result.res.statusCode, result.data.length);
+            console.info('[WIKI] Parameters: %s, Status: %s, Size: %d', query, result.res.statusCode, result.data.length);
             if(result.res.statusCode > 400) {
                 reply = "Page doesn't exist.";
             }
@@ -34,18 +34,36 @@ const search = async (wikiRootUrl, query, filter = " - ") => {
                 reply = title + " | " + encodedUrl;
             }
             return reply;
-        }).catch(function (err) {
+        })
+        .catch(function (err) {
             console.log(err);
             return "Exception thrown: " + err.message;
         });
     }
 }
 
+exports.run = async (client, message, args) => {
+    try {
+        //Reformat the arguments:
+        let context = (([url, ...others]) => ({"url": url, "query": [...others].join('_')}))(args);
+        let result = await search(context.url, context.query);
+        message.channel.send(result);
+    }
+    catch (e) {
+        message.channel.send(e);
+    }
+};
+
+///beep wiki http://gbf.wiki/ Ultima_Sword
+exports.command = {
+    "alias": "wiki",
+    "description": "Searches MediaWiki websites."
+}
+
 function makeWikiTitle(title, filter, emoji = "\:book:")
 {
     //Expected input: %s %s %s
+    console.info(`[WIKI] Page title: ${title}`);
     const editedTitle = emoji + " " + title.substring(title.lastIndexOf(filter) + filter.length) + ": " + title.substring(0, title.lastIndexOf(filter));
     return editedTitle;
 }
-
-exports.search = search;
